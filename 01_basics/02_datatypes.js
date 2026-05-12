eda4fcd0a0df98c557124cee964ee3745db8b524
NikitaR04 <nikita4321rani@gmail.com>
2026-05-12 05:18:11 +0000

    Datatypes basics

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using nodejs, not browser where alert syntax is valid!
+
+//Readability of code is very important!
+
+//DataTypes
+
+let name = "Nikita"
+let age = 18
+let isLoggedIn = false
+
+//Primitive datatypes:
+
+//number => (range) 2 to power 53
+//bigint (used for very large no. mostly in stock/trading/facebook)
+//string => ""
+//boolean => true/false
+//null (standalone value) => empty value
+//undefined (unassigned value)
+//symbol => unique
+
+//object
+
+console.log(typeof "Nikita", typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
